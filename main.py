584fb3f5739e9996db483f67ccdb17e7bec30382
Cactus-John <ivan.ivo.bosnjak@gmail.com>
2023-01-22 11:20:37 +0100

    get amazon url

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from tkinter import *
-
-
-def click():
-    print("You clicked the button")
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
 
 
 def openNewWindow():
@@ -21,15 +20,58 @@ def openNewWindow():
     Label(newWindow, text="This is your store! Here are your items and products").pack()
 
 
+def openShopWindow():
+    newShopWindow = Toplevel(master)
+    newShopWindow.title("Shop")
+    newShopWindow.geometry("1500x600")
+    newShopWindow.resizable(False, False)
+    newShop_exit_button = Button(newShopWindow, text="Close",
+                                 command=newShopWindow.destroy,
+                                 fg="red",
+                                 bg="#E9CE2C",
+                                 activeforeground="red",
+                                 activebackground="#E9CE2C",
+                                 state="active")
+    newShop_exit_button.pack(side=BOTTOM)
+    Label(newShopWindow, text="Welcome! This is your shopping window").pack()
+
+
+def goShopping():
+    shopping_button = Button(openShopWindow, text="Go shopping",
+                            command=openShopWindow,
+                            width=10,
+                            fg="#00FF00",
+                            bg="black",
+                            activeforeground="#00FF00",
+                            activebackground="black",
+                            state="active")
+    shopping_button.pack(pady=15)
+    browser = webdriver.Chrome(ChromeDriverManager().install())
+    browser.get('https://www.amazon.de/')
+    shopping_button = browser.find_element_by_link_text('Best Sellers')
+    shopping_button.click()
+    time.sleep(3)
+
+
 master = Tk()
-master.geometry("500x200")
+master.geometry("500x500")
 master.resizable(False, False)
 
 label = Label(master, text="Home page")
 label.pack(side=TOP, pady=10)
 
-btn = Button(master, text="Click to open your store of items and products",
+shop_button = Button(master, text="SHOP",
+                     command=openNewWindow,
+                     fg="#00FF00",
+                     bg="black",
+                     activeforeground="#00FF00",
+                     activebackground="black",
+                     state="active")
+shop_button.pack(pady=15)
+
+btn = Button(master, text="STORE",
              command=openNewWindow,
+             width=6,
              fg="#00FF00",
              bg="black",
              activeforeground="#00FF00",
@@ -44,6 +86,5 @@ exit_button = Button(master, text="Close",
                      activeforeground="red",
                      activebackground="#E9CE2C")
 exit_button.pack(side=BOTTOM, pady=10)
-
 
 mainloop()
