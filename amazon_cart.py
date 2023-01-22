b7697cd9da8a560530ca561e5450e79b5b321279
Cactus-John <ivan.ivo.bosnjak@gmail.com>
2023-01-22 18:46:54 +0100

    fixed the position of the button

--- amazon_cart.py
+++ amazon_cart.py
@@ -2,7 +2,7 @@ import pyautogui
 import time
 import webbrowser
 
-cart_position = 1662, 619
+cart_position = 1621, 617
 url = 'https://www.amazon.de/-/en/Weeknd/dp/B084XTMZVS/ref=sr_1_2?crid=3G0H4SQA4VZTI&keywords=after+hours+vinyl&qid=1674408728&sprefix=after+h%2Caps%2C361&sr=8-2'
 webbrowser.open(url)
 time.sleep(5)
